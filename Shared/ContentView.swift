bc8ec5f7a704c7e6d83757b5f54714e476854b9b
JeanDNM <jeanneyra06@gmail.com>
2024-05-19 15:08:42 -0700

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Jeanpierre on 30/02/1403 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/tareadefrasesApp.swift
@@ -0,0 +1,17 @@
+//
+//  tareadefrasesApp.swift
+//  Shared
+//
+//  Created by Jeanpierre on 30/02/1403 AP.
+//
+
+import SwiftUI
+
+@main
+struct tareadefrasesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
